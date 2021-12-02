b2ab5aedc8c90f634e004595e46a06150675b485
Dan Thornton <dan.thornton+git@thornton.me>
2021-12-02 03:54:28 -0500

    day 2

--- src/day2.rs
+++ src/day2.rs
@@ -6,14 +6,16 @@
 /// https://docs.rs/regex/1.4.2/regex/#syntax 
 extern crate regex;
 // use self::regex::{Captures, Regex};
+use std::convert::From;
+use self::Movement::*;
 
 // ********************
 // *** Generator(s) ***
 // ********************/
 #[aoc_generator(day2)]
-pub fn gen1(input: &str) -> Vec<u16> {
+pub fn gen1(input: &str) -> Vec<Movement> {
     input.lines()
-        .map(|line|line.parse().unwrap())
+        .map(|line|Movement::from(line))
         .collect()
 }
 
@@ -21,14 +23,48 @@ pub fn gen1(input: &str) -> Vec<u16> {
 // *** Part1 & Part2 ***
 // *********************
 #[aoc(day2, part1)]
-pub fn part1(input: &Vec<u16>) -> usize {
-    0
+pub fn part1(input: &Vec<Movement>) -> i32 {
+    let finalpos = input.into_iter()
+        .fold((0,0),|(xpos,depth), mv| match mv {
+            Forward(n) => (xpos+*n, depth),
+            Up(n) => (xpos, if depth >= *n {depth-*n} else {0}),
+            Down(n) => (xpos, depth+*n)
+        });
+    finalpos.0*finalpos.1
 }
 
+
 #[aoc(day2, part2)]
-pub fn part2(input: &Vec<u16>) -> usize {
-    0
+pub fn part2(input: &Vec<Movement>) -> i32 {
+    let finalpos = input.into_iter()
+    .fold((0,0,0i32),|(xpos,depth,aim), mv| match mv {
+        Forward(n) => (xpos+*n, depth+aim**n, aim),
+        Up(n) => (xpos, depth, aim-*n),
+        Down(n) => (xpos, depth, aim+*n)
+    });
+    finalpos.0*finalpos.1
 }
+
+pub enum Movement {
+    Forward(i32),
+    Up(i32),
+    Down(i32)
+}
+
+impl From<&str> for Movement {
+    fn from(s: &str) -> Self {
+        let mut half = s.split(" ");
+        let cmd = half.next().unwrap();
+        let dist = half.next().unwrap().parse().unwrap();
+        match cmd {
+            "forward" => Forward(dist),
+            "up" => Up(dist),
+            "down" => Down(dist),
+            _ => panic!("Parse error on Movement")
+        }
+    }
+}
+
 
 // *************
 // *** Tests ***
