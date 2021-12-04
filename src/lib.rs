5f2e21fa5965c08660c51852df1c4d05b93b417b
Dan Thornton <dan.thornton+git@thornton.me>
2021-12-04 16:13:58 -0500

    day4

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ extern crate aoc_runner;
 extern crate aoc_runner_derive;
 extern crate crypto;
 
-pub mod day1;
-pub mod day2;
-pub mod day3;
+// pub mod day1;
+// pub mod day2;
+// pub mod day3;
 pub mod day4;
 // pub mod day5;
 // pub mod day6;
