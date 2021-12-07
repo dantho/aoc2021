5fc5213f50b8ab3ac20e36e06fa5f878a312dc24
Dan Thornton <dan.thornton+git@thornton.me>
2021-12-07 06:38:55 -0500

    day7

--- src/day7.rs
+++ src/day7.rs
@@ -10,22 +10,48 @@
 // *** Generator(s) ***
 // ********************/
 #[aoc_generator(day7)]
-pub fn gen1(input: &str) -> () {
+pub fn gen1(input: &str) -> Vec<usize> {
+    input.split(",").map(|numstr|numstr.parse().unwrap()).collect()
 }
 
 // *********************
 // *** Part1 & Part2 ***
 // *********************
 #[aoc(day7, part1)]
-pub fn part1(input: &()) -> usize {
-    0
+pub fn part1(input: &[usize]) -> usize {
+    let max_pos = *input.iter().max().unwrap();
+    let align_energy: Vec<usize> = (0..=max_pos)
+        .map(|target_pos|{
+            input.iter().map(|&p|(if p>target_pos {p-target_pos} else {target_pos-p})).sum()
+        }).collect();
+    align_energy.iter().enumerate().fold((usize::MAX,usize::MAX),|(min_ndx,min_e),(ndx,&e)|{
+        if e < min_e {
+            (ndx,e)
+        } else {
+            (min_ndx,min_e)
+        }
+    }).1
 }
 
 #[aoc(day7, part2)]
-pub fn part2(input: &()) -> usize {
-    0
+pub fn part2(input: &[usize]) -> usize {
+    let max_pos = *input.iter().max().unwrap();
+    let align_energy: Vec<usize> = (0..=max_pos)
+        .map(|target_pos|{
+            input.iter().map(|&p|(geom(if p>target_pos {p-target_pos} else {target_pos-p}))).sum()
+        }).collect();
+    align_energy.iter().enumerate().fold((usize::MAX,usize::MAX),|(min_ndx,min_e),(ndx,&e)|{
+        if e < min_e {
+            (ndx,e)
+        } else {
+            (min_ndx,min_e)
+        }
+    }).1
 }
 
+fn geom(dist:usize)->usize {
+    (1..=dist).sum()
+}
 // *************
 // *** Tests ***
 // *************
@@ -34,18 +60,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_ex1_part1() {
-        assert_eq!(888, 999);
+    fn test_gen1() {
+        let g = gen1(EX1);
+        assert_eq!(g.len(), 10);
     }
 
     #[test]
-    fn test_ex2_part2() {
-        assert_eq!(888, 999);
+    fn test_ex1_part1() {
+        assert_eq!(part1(&gen1(EX1)), 37);
+    }
+
+    #[test]
+    fn test_ex1_part2() {
+        assert_eq!(part2(&gen1(EX1)), 168);
     }
 
 const EX1: &'static str =
-r"
-";
+r"16,1,2,0,4,2,7,1,2,14";
 
 const EX2: &'static str =
 r"
